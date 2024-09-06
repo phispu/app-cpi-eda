8780b1a82383fcf9b63f40905a677e872e6e15f6
jrdusendang <jdusendang@graticule.life>
2024-09-05 17:58:38 -0700

    fix api_data_pull_dev

--- code/api_data_pull_dev.R
+++ code/api_data_pull_dev.R
@@ -8,23 +8,32 @@ install.packages(c('httr', 'jsonlite', 'dplyr', 'renv'))
 library('httr')
 library('jsonlite')
 library('dplyr')
+library(rio)
 
 #specify the metric, MSA, and table ID (from FRED) for the table to pull 
-import_table_names = list(
-  c(metric = 'CPI',     msa = 'SF',   lookup_id = 'CUURA422SA0'),
-  c(metric = 'CPI',     msa = 'NY',   lookup_id = 'CUURA101SA0'),
-  c(metric = 'Income',  msa = 'SF',   lookup_id = 'SANF806PCPI'), 
-  c(metric = 'Income',  msa = 'NY',   lookup_id = 'NEWY636PCPI'))
+import_table_names = import('~/app-cpi-eda/data/fred_dataset_lookup.csv') |>
+  filter(!is.na(lookup_id) & lookup_id != '') 
 
 #create an empty data frame 
 build_df = data.frame(metric=character(), msa=character(), date=as.Date(character()), 
                       value=numeric())
 
-#loop through all import tables and pull, clean, and stack data 
-for (item in import_table_names) {
+delay_seconds = 0.7
+
+#loop through all import tables and pull, clean, and stack data
+
+for (i in 1:nrow(import_table_names)) {
+  
+  curr_time = as.numeric(Sys.time())
+  
+  metric = import_table_names[i, 1]
+  msa = import_table_names[i, 2]
+  table_id = import_table_names[i, 3]
+  
+  print(paste0('Loading: ', metric, ' for ', msa))
   
   #api call for the data 
-  api_call = GET(paste0('https://api.stlouisfed.org/fred/series/observations?series_id=', item[[3]], 
+  api_call = GET(paste0('https://api.stlouisfed.org/fred/series/observations?series_id=', table_id, 
                         '&api_key=', Sys.getenv("FRED_API_KEY"), '&file_type=json'))
   
   #separate the data from metadata in api call 
@@ -32,16 +41,19 @@ for (item in import_table_names) {
   
   #structure the data for stacking 
   clean_data = pull_data |>
-    mutate(metric = item[[1]],
-           msa = item[[2]],
+    filter(!is.na(value)) |>
+    mutate(metric = metric,
+           msa = msa,
            value = as.numeric(value)) |>
     select(metric, msa, date, value) |> 
     filter(!is.na(value))
-
+  
   #stack the data to the base dataframe 
   build_df = build_df |>
     rbind(clean_data) 
-
+  
+  while((as.numeric(Sys.time()) - as.numeric(curr_time)) < delay_seconds){}
+  
 }
 
 #save to data folder 
